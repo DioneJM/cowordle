6d900150de1347659c3475204b03a371219e6599
Dione <dionemorales@outlook.com>
2022-03-13 18:17:26 +1100

    Added board

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import styled from 'styled-components'
 
-
-const Board = () => {
-  return <div />
+export interface BoardProps {
+  dimensions: {
+    width: number,
+    height: number
+  }
 }
+
+const BoardWrapper = styled.div`
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Board = ({ dimensions }: BoardProps) => {
+  return <BoardWrapper>
+    {Array.from(Array(dimensions.height).keys()).filter(row => row != undefined).map((row) => {
+      return <Row key={row} isFirst={row === 0}>
+        {Array.from(Array(dimensions.width).keys()).filter(column => column != undefined).map((column) => (
+          <Block key={`${row},${column}`} isFirst={column === 0}>{`${row},${column}`}</Block>
+        ))}
+        <br />
+      </Row>
+    })}
+  </BoardWrapper>
+}
+
+const Row = styled.div<{ isFirst: boolean }>`
+  display: flex;
+  margin-top: ${({ isFirst }) => isFirst ? '0' : '4'}px;
+`
+
+const Block = styled.div<{ isFirst: boolean }>`
+  width: 62px;
+  height: 62px;
+  border: 2px solid rgb(58, 58, 60);
+  color: white;
+  display: flex;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  margin-left: ${({ isFirst }) => isFirst ? '0' : '4'}px;
+`
 
 export default Board
--- src/CoWordle.tsx
+++ src/CoWordle.tsx
@@ -8,13 +8,30 @@ const Wrapper = styled.div`
   background-color: rgb(18, 18, 19);
   height: 100vh;
   width: 100vw;
+  min-width: 350px;
+  min-height: 450px;
+  overflow: auto;
+`
+
+const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 1rem;
 `
 
 const CoWordle = () => {
   return <Wrapper>
     <Header />
-    <Board />
-    <Keyboard />
+    <Content>
+      <Board dimensions={{
+        width: 5,
+        height: 5,
+      }} />
+      <Keyboard />
+    </Content>
   </Wrapper>
 }
 
